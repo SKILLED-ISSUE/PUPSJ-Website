12e0dd122630761821eba3e5cc87f96e4eec7ed0
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-24 23:29:16 +0800

    Update js

--- main-script.js
+++
@@ -1,9 +0,0 @@
-var loader = document.getElementById("preloader");
-
-window.addEventListener("load", function () {
-  loader.classList.add("fade-out");
-
-  loader.addEventListener("transitionend", function () {
-    loader.style.display = "none";
-  });
-});
